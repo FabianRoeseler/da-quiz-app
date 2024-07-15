8a3f64d2426672a7fb7fa7da3f4faea26da40cbe
crimme <kueppers.fabian@web.de>
2024-07-15 14:38:30 +0200

    updated version

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ let questions = [
     right_answer: 3,
   },
   {
-    question: "Woe bindet man eine Website in eine Website ein?",
+    question: "Wie bindet man eine Website in eine Website ein?",
     answer_1: "falsch",
     answer_2: "richtig",
     answer_3: "falsch",
@@ -41,22 +41,51 @@ function init() {
 }
 
 function showQuestion() {
-  let question = questions[currentQuestion];
+  if (currentQuestion >= questions.length) {
+    document.getElementById("endScreen").style = "";
+    document.getElementById("questionBody").style = "display: none";
+  } else {
+    let question = questions[currentQuestion];
 
-  document.getElementById("questiontext").innerHTML = question["question"];
-  document.getElementById("answer_1").innerHTML = question["answer_1"];
-  document.getElementById("answer_2").innerHTML = question["answer_2"];
-  document.getElementById("answer_3").innerHTML = question["answer_3"];
-  document.getElementById("answer_4").innerHTML = question["answer_4"];
+    document.getElementById("question-number").innerHTML = currentQuestion + 1;
+    document.getElementById("questiontext").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+  }
 }
 
 function answer(selection) {
   let question = questions[currentQuestion];
   let selectedQuestionNumber = selection.slice(-1);
+  let idOfRightAnwser = `answer_${question["right_answer"]}`;
 
   if (selectedQuestionNumber == question["right_answer"]) {
-    console.log("Right answer");
+    document.getElementById(selection).parentNode.classList.add("bg-success");
   } else {
-    console.log("Wrong answer");
+    document.getElementById(selection).parentNode.classList.add("bg-danger");
+    document
+      .getElementById(idOfRightAnwser)
+      .parentNode.classList.add("bg-success");
   }
+  document.getElementById("next-button").disabled = false;
+}
+
+function nextQuestion() {
+  currentQuestion++;
+  document.getElementById("next-button").disabled = true;
+  resetAnswerButtons();
+  showQuestion();
+}
+
+function resetAnswerButtons() {
+  document.getElementById("answer_1").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_1").parentNode.classList.remove("bg-success");
+  document.getElementById("answer_2").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_2").parentNode.classList.remove("bg-success");
+  document.getElementById("answer_3").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_3").parentNode.classList.remove("bg-success");
+  document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_4").parentNode.classList.remove("bg-success");
 }
